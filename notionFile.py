5cd1684cdf1e4293256e4fd2bcbd939fbae89f6f
Rithin Lehan <74766580+Rithingithub@users.noreply.github.com>
2022-05-07 22:38:00 +0530

    Update notionFile.py

--- notionFile.py
+++ notionFile.py
@@ -1,19 +1,85 @@
 import json
 import requests
 
-notion_token = '<API_TOKEN>'
-DBid = '3c35a8a4641e441c9fafff1dca38cd26'
+notion_token = '<API_KEY>'
+DBid = '<ID>'
+
 
 headers = {
       "Notion-Version": "2021-05-13",
       "Content-Type": "application/json",
       "Authorization": "Bearer " + notion_token
     }
-    #self.base_url = "https://api.notion.com/v1"
- 
+   #self.base_url = "https://api.notion.com/v1"
+
+
 def readDB(DBid):
     readUrl = f"https://api.notion.com/v1/databases/{DBid}/query"
 
     res = requests.request("POST", readUrl, headers=headers)
     data = res.json()
     print(res.status_code)
+
+
+def createPage(DBid, headers):
+    createUrl = 'https://api.notion.com/v1/pages'
+
+    newPageData = {
+        "parent": {"database_id": DBid},
+        "properties": {
+            "Description": {
+                "title": [
+                    {
+                        "text": {
+                            "content": "Review"
+                        }
+                    }
+                ]
+            },
+            "Value": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Amazing"
+                        }
+                    }
+                ]
+            },
+            "Status": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Active"
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+    data = json.dumps(newPageData)
+
+    res = requests.request("POST", createUrl, headers=headers, data=data)
+    print(res.text)
+
+def updatePage(padeId, headers):
+    updateUrl = f"https://api.notion.com/v1/pages/{pageId}"
+
+    updateData = {
+        "properties": {
+            "Value": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": "Pretty Good"
+                        }
+                    }
+                ]
+            }        
+        }
+    }
+
+    data = json.dumps(updateData)
+
+    response = requests.request("PATCH", updateUrl, headers=headers, data=data)
+    print(response.text)
