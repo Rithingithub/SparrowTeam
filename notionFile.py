ff98365ab6557df45eb8ae570fbbfd2c7e061918
Rithin Lehan <74766580+Rithingithub@users.noreply.github.com>
2022-05-07 21:14:11 +0530

    Update notionFile.py

--- notionFile.py
+++ notionFile.py
@@ -2,13 +2,18 @@ import json
 import requests
 
 notion_token = '<API_TOKEN>'
+DBid = '3c35a8a4641e441c9fafff1dca38cd26'
 
-
-class Notion:
-
-  def __init__(self, notion_token):
-    self.headers = {
-      'Notion-Version': '2021-05-13',
-      'Authorization': 'Bearer ' + notion_token
+headers = {
+      "Notion-Version": "2021-05-13",
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + notion_token
     }
-    self.base_url = "https://api.notion.com/v1"
+    #self.base_url = "https://api.notion.com/v1"
+ 
+def readDB(DBid):
+    readUrl = f"https://api.notion.com/v1/databases/{DBid}/query"
+
+    res = requests.request("POST", readUrl, headers=headers)
+    data = res.json()
+    print(res.status_code)
